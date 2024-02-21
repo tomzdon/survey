8538d19aa0313fe0895e20f55871a338c41ec138
tomasz.wlodarczyk_di <tomasz.wlodarczyk@dietly.pl>
2024-02-21 14:29:47 +0100

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const API_KEY = 'YOUR_API_KEY';
+
+app.post('/track', (req, res) => {
+
+  console.log(req)
+  res.sendStatus(200);
+});
+
+let server = app.listen(3000, () => {
+  console.log('Listening on port %d', server.address().port);
+});
